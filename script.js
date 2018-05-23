4d228521d2ee63dbba1e76213067892fb19b069c
Hossain Ahmed <ahmed-hossain2@live.com>
2018-05-23 03:04:05 +0000

    task 5

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 /* JavaScript code here */
+var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+   
+   //Task 5
+   
+   function addItem(item) {
+       cart.push(item);
+       console.log(cart);
+   }
+   var newItem = {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   };
+   addItem(newItem);
+   
